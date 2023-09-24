00559d3d76717154e9fc444cd84f25daf5e09ccc
srgowen <srgowen2@gmail.com>
2023-09-24 12:25:06 -0400

    changed the var to const since the variables wont change

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,20 @@
 // Get references to the #generate element and textarea
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
 
 // Function to generate the random password
 function generatePassword() {
   // Possible characters in password
-  var charset = "";
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numericChars = "0123456789";
-  var specialChars = "!@#$%^&*()-_=+[]{}|;:'\"<>,.?/";
+  const charset = "";
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars = "0123456789";
+  const specialChars = "!@#$%^&*()-_=+[]{}|;:'\"<>,.?/";
 
-  // Promts the user to enter password length
-  var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters):"));
+  // Prompts the user to enter password length
+  const passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters):"));
 
-  // Validates the user inputed password length
+  // Validates the user inputted password length
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length between 8 and 128 characters.");
     return "";
@@ -38,9 +38,9 @@ function generatePassword() {
   }
 
   // Generates the password based on the conditions set above
-  var password = "";
-  for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset.charAt(randomIndex);
   }
 
@@ -49,11 +49,9 @@ function generatePassword() {
 
 // Writes the generated password to the black space
 function writePassword() {
-  var password = generatePassword();
+  const password = generatePassword();
   passwordText.value = password;
 }
 
 // Makes the button interactive to start the password generation
 generateBtn.addEventListener("click", writePassword);
-
-
